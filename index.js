897ebe682f1d05a59b34fb11bb8135231459b6cd
Erick Castille <ecastille924@gmail.com>
2022-01-31 19:43:25 -0500

    working out session storage for user selection:

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    fetchType()
+    // fetchType()
+    dropdownSelect()
 })
 
+
+function dropdownSelect(){
+    let userSelection = document.getElementById("type-select")
+    userSelection.addEventListener("change",function(){
+        sessionStorage.setItem("selection", userSelection.value),
+        location.reload()
+    })
+    console.log(sessionStorage.selection)
+}
+
 function fetchType() {
+    let userSelection = document.getElementById("type-select").value
     let output = document.getElementById("type-info")
     fetch("https://pokeapi.co/api/v2/type/1")
     .then(resp => resp.json())
     .then(json => 
-        console.log(json.damage_relations.double_damage_from[0].name,
-            json.damage_relations.half_damage_to[0].name)
+        console.log(json.damage_relations.double_damage_from[userSelection].name,
+            json.damage_relations.half_damage_to[userSelection].name)
         // output.innerHTML = (json.damage_relations.double_damage_from)
         )
+        console.log(userSelection)
 }
 
-// function renderPokeData(data){
-//     let output = document.getElementById("type-info")
 
-// }
+
+// if user slection = [x] 
+//     fetch url[x]
