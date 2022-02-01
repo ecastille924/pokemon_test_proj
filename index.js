ac9b30367ebc41f23fda481da7aa9b2bd7f56d3e
Erick Castille <ecastille924@gmail.com>
2022-01-31 19:15:42 -0500

    setting up initial fetch call and dropdown for type select

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ document.addEventListener("DOMContentLoaded", () =>{
 })
 
 function fetchType() {
-    fetch("https://pokeapi.co/api/v2/type")
+    let output = document.getElementById("type-info")
+    fetch("https://pokeapi.co/api/v2/type/1")
     .then(resp => resp.json())
-    .then(json => console.log(json.results[0]))
+    .then(json => 
+        console.log(json.damage_relations.double_damage_from[0].name,
+            json.damage_relations.half_damage_to[0].name)
+        // output.innerHTML = (json.damage_relations.double_damage_from)
+        )
 }
+
+// function renderPokeData(data){
+//     let output = document.getElementById("type-info")
+
+// }
