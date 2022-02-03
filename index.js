84a413133e3b58f56c9962e6c8b4d2a01089c96e
Erick Castille <ecastille924@gmail.com>
2022-02-02 20:25:32 -0500

    working out dymanc fetch based on user input

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    // fetchType()
     dropdownSelect()
 })
 
 
 function dropdownSelect(){
     let userSelection = document.getElementById("type-select")
+
     userSelection.addEventListener("change",function(){
         sessionStorage.setItem("selection", userSelection.value),
         location.reload()
     })
-    console.log(sessionStorage.selection)
+    // console.log(sessionStorage.selection)
     fetchType(sessionStorage.selection)
 }
 
-function fetchType(index) {
+function fetchType() {
+    
     let baseurl = "https://pokeapi.co/api/v2/type/"
+    let userSelectedIndex = sessionStorage.selection 
     let output = document.getElementById("type-info")
-    fetch(baseurl)
+    
+    fetch(baseurl.concat(userSelectedIndex))
     .then(resp => resp.json())
     .then(json => 
-        // console.log(json.results[index]),
-        output.innerHTML = (json.results[index].name)
+        console.log(json.damage_relations.double_damage_from)
+        // output.innerHTML = (json.results)
         )
+        // console.log(baseurl.concat(userSelectedIndex))
 }
 
 
 
-// if user slection = [x] 
-//     fetch url[x]
+
+// if user selection = x 
+// fetch base url / x + 1 
+
+
